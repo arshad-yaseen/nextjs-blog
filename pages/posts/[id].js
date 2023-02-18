a66260ce7c472158e11b8f7d94444bab2a43fe57
Glowit Labs <glowitsofware@gmail.com>
2023-02-18 16:26:04 +0530

    Added metas to post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,8 @@
-import Layout from "../components/Layout.js";
+import Layout,{ siteTitle } from "../components/Layout.js";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import utilStyles from "../../styles/utils.module.css";
+import profilePic from "../../public/images/profile.jpg"
 
 export async function getStaticPaths() {
   const paths = getAllPostIds();
@@ -23,8 +24,18 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      <Head>
-        <title>{postData.title}</title>
+       <Head>
+        <link rel="icon" href="https://pbs.twimg.com/profile_images/1618614145666142211/EM22F0FG_400x400.jpg" />
+        <meta
+          name="description"
+          content={"A blog from " + siteTitle + " - " + postData.date }
+        />
+        <meta
+          property="og:image"
+          content={profilePic}
+        />
+        <meta name="og:title" content={postData.title} />
+        <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>{postData.title}</h1>
